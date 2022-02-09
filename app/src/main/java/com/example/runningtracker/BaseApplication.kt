b9f77625c9b9f977629d17aa56cee4c07744e1f5
Yujin <sofod12@naver.com>
2022-02-09 22:30:41 +0900

    Part 01 ~ 06

---
+++ app/src/main/java/com/example/runningtracker/BaseApplication.kt
@@ -0,0 +1,9 @@
+package com.example.runningtracker
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class BaseApplication : Application() {
+
+}
---
+++ app/src/main/java/com/example/runningtracker/other/Constants.kt
@@ -0,0 +1,5 @@
+package com.example.runningtracker.other
+
+object Constants {
+    const val RUNNING_DATABASE_NAME = "running_db"
+}
